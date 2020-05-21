9e8c2a3ac7657ee50cec1d91caa8ff65564dfd3e
shaggy611 <sevczik11@gmail.com>
2020-05-21 10:53:27 +0300

    bug fix complete

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
+    $(function() {
+        $(document).click(function (event) {
+          $('.navbar-collapse').collapse('hide');
+        });
+      });
+
     let flag = false;
     document.addEventListener ('scroll', function() {
         if (flag) return;
         $('#modal_veepn').modal('show');
         flag = true;
-    });    
-});     
+    });   
+    
+    document.addEventListener ('scroll', function() {
+        let menu = $('#navbarNavAltMarkup');
+        let check_menu_show = menu.hasClass('collapse navbar-collapse menu__links_margin');
+        $('.navbar-collapse').collapse('hide');
+        
+       
+    }); 
+});  
